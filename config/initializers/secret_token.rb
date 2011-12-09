92cec44f3ba38466c5ef14bb8eaece9d83eaa5ba
Giada Barbini <jadefkriv@hotmail.com>
2011-12-08 20:27:06 -0800

    new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OneThousandDays::Application.config.secret_token = 'f8e9d41834c2a994b3778f14870deb095bfbb2595338f9d383002226f181bbcb1d596968fb7cf242749163d82dc4b7bcff6db63a62f2c563880867ae5026e303'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+OneThousandDays::Application.config.session_store :cookie_store, key: '_one-thousand-days_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# OneThousandDays::Application.config.session_store :active_record_store
